61224364e505f1a76859bcdf935684fdae36ddee
Unknown <misskristinablinova@gmail.com>
2017-09-30 17:27:02 +0100

    Quaternio lerp added
    Camera gently rotates depending on a destination

--- Assets/Scripts/Camera/cameraOnrails.cs
+++ Assets/Scripts/Camera/cameraOnrails.cs
@@ -4,19 +4,24 @@ using UnityEngine;
 
 public class cameraOnrails : MonoBehaviour {
 
-    public GameObject destinationCollection;
-    private Transform[] destinations;
+    //--------VARIABLES---------
+    public GameObject destinationCollection;    //Collection of the  game objects representing destinations
+    private Transform[] destinations;           //Array of transforms, which serve as destinations
 
-    private int nextDestination =0;
-    private bool canMove = true;
+    private int nextDestination =0;             //Indicates a destination camera is moving towards
+    private bool canMove = true;                //Indicates if camera can move
 
+    public float rotationSpeed = 2.0f;          //Speed of quaternion lerp used in rotation
+
+    //-------CONSTRUCTOR--------
     private void Start()
     {
+        //Define size of the "destination" array
         int arrayLength = destinationCollection.transform.childCount;
         destinations = new Transform[arrayLength];
 
+        //Populate "destination" array
         int nextObject = 0;
-
         foreach (Transform child in destinationCollection.transform)
         {
             destinations[nextObject] = child;
@@ -24,18 +29,20 @@ public class cameraOnrails : MonoBehaviour {
         }
     }
 
+    //------FUNCTIONALITY--------
+
+    //Changes destination of the camera, is called by destination prefabs
     public void changeDestination()
     {
         if (destinations.Length > nextDestination + 1)
         {
             nextDestination++;
-            transform.rotation = destinations[nextDestination].transform.rotation;
         }
         else
             canMove = false;
     }
 	
-	// Update is called once per frame
+	// Update, currently handles player input
 	void Update ()
     {
         if (Input.GetKey("space"))
@@ -46,13 +53,15 @@ public class cameraOnrails : MonoBehaviour {
 
 	}
 
-    //Move to the next position
+    //Move forward to the next position
     void MoveForward()
     {
         transform.position = Vector3.Lerp(transform.position, destinations[nextDestination].transform.position, Time.deltaTime);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, destinations[nextDestination].transform.rotation, Time.deltaTime * rotationSpeed);
     }
 
-    //Move to the previous position
+    //Move back to the previous position
     void MoveBackwards()
     {
 
