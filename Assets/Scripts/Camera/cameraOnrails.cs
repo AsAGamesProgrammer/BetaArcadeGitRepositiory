17d5950ad71b7ec9005b799d83394f2943d4c314
Unknown <misskristinablinova@gmail.com>
2017-09-30 16:29:20 +0100

    Basic Camera on rails
    Scene 1 has a basic camera on rails set up

---
+++ Assets/Scripts/Camera/cameraOnrails.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraOnrails : MonoBehaviour {
+
+    public GameObject[] destinations;
+
+    private int nextDestination =0;
+    private bool canMove = true;
+
+
+    public void changeDestination()
+    {
+        if (destinations.Length > nextDestination + 1)
+        {
+            nextDestination++;
+            transform.rotation = destinations[nextDestination].transform.rotation;
+        }
+        else
+            canMove = false;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey("space"))
+        {
+            if (canMove)
+                MoveForward();
+        }
+
+	}
+
+    //Move to the next position
+    void MoveForward()
+    {
+        transform.position = Vector3.Lerp(transform.position, destinations[nextDestination].transform.position, Time.deltaTime);
+    }
+
+    //Move to the previous position
+    void MoveBackwards()
+    {
+
+    }
+}
