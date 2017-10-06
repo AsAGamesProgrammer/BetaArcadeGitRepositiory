6afe0d615875b5042b4f2c671edbb957c2a46bc5
Unknown <craigbelshaw0@gmail.com>
2017-10-06 14:40:13 +0100

    Added script for Async
    basic version to show how the asyn would work

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+
+  public bool transitionIsDone;
+  public string nextSceneName;
+  AsyncOperation async;
+  void Start () {
+  }
+
+  public IEnumerator ChangeSceneHelper(string sceneName)
+  {
+
+    Scene oldScene = SceneManager.GetActiveScene();
+    Scene newScene = SceneManager.GetSceneByName(sceneName);
+
+    Camera oldMainCamera = Camera.main;
+    Camera.main.tag = "Untagged";
+    async = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
+    while (!async.isDone)
+    {
+
+      float x = async.progress;
+      if (async.progress > 0.9f)
+      {
+        FindObjectOfType<AudioListener>().enabled = false;
+        oldMainCamera.gameObject.SetActive(false);
+        foreach (var go in oldScene.GetRootGameObjects())
+        {
+          go.SetActive(false);
+          Destroy(go);
+        }
+        yield return null;
+      }
+      SceneManager.SetActiveScene(newScene);
+    }
+  }
+
+  void Update () {
+    if (transitionIsDone)
+    {
+      StartCoroutine(ChangeSceneHelper(nextSceneName));
+      transitionIsDone = false;
+      SceneManager.sceneCount.ToString();
+    } 
+  }
+}
