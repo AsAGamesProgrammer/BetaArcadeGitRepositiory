d998aae0262e15a89dbeefc0a4f34508956f79a5
Unknown <craigbelshaw0@gmail.com>
2017-10-08 17:24:06 +0100

    start on light
    starting on the light puzzle

---
+++ Assets/Scripts/LightObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightObject : MonoBehaviour {
+
+  public float TimeOfLight;
+  public float Intensity;
+  public bool LightOn;
+  float TimeOfRun;
+
+	// Use this for initialization
+	void Start () {
+    Light objectLight = GetComponent<Light>();
+    if (objectLight != null)
+    {
+      objectLight.enabled = LightOn;
+      objectLight.intensity = Intensity;
+      TimeOfRun = TimeOfLight;
+    }
+  }
+	
+	// Update is called once per frame
+	void Update () {
+    Light objectLight = GetComponent<Light>();
+  }
+}
