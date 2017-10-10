37b90e9ccfb909b7ad0575d0834ed1d9e7cd662f
Craig Belshaw <craigbelshaw0@gmail.com>
2017-10-10 12:53:40 +0100

    Light Puzzel
    Got the light to reduce based on time and started on activation via collision

---
+++ Assets/Scripts/LightObjectScripts/LightCollide.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightCollide : MonoBehaviour {
+
+
+  public GameObject Player;
+  public GameObject LightObject;
+  // Use this for initialization
+  void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+  }
+
+  void OnCollisionEnter(Collider other)
+  {
+    if (other.name == Player.name)
+    {
+      
+    }
+  }
+
+  void OnCollisionExit(Collider other)
+  {
+    if (other.name == "Detection Zone")
+    {
+      Debug.Log("leavingtrigger");
+      inSight = false;
+      Debug.Log("not in sight");
+    }
+  }
+}
---
+++ Assets/Scripts/LightObjectScripts/LightObject.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightObject : MonoBehaviour
+{
+
+  public int TimeOfLight;
+  public float Intensity;
+  public bool LightOn;
+  public float LightRange;
+  float TimeOfRun;
+
+    // Use this for initialization
+  void Start ()
+  {
+    Light objectLight = GetComponent<Light>();
+    if (objectLight != null && LightOn)
+    {
+      objectLight.enabled = LightOn;
+      objectLight.intensity = Intensity;
+      objectLight.range = LightRange;
+      StartCoroutine(TimerCount(objectLight));
+    }
+  }
+
+
+  private IEnumerator TimerCount( Light objectLight)
+  {
+    while (TimeOfRun > 0)
+    {
+      if(objectLight.range > 0)
+        objectLight.range -= LightRange/TimeOfRun;
+      yield return new WaitForSeconds(1);
+    }
+    if(TimeOfRun >= 0)
+    {
+      objectLight.enabled = false;
+      objectLight.intensity = Intensity;
+      TimeOfRun = TimeOfLight;
+    }
+  }
+  // Update is called once per frame
+  void Update ()
+  {
+    Light objectLight = GetComponent<Light>();
+    if (objectLight == null)
+      return;
+    if (LightOn)
+    {
+      objectLight.enabled = LightOn;
+      objectLight.intensity = Intensity;
+      objectLight.range = LightRange;
+      TimeOfRun = TimeOfLight;
+      LightOn = false;
+      StartCoroutine(TimerCount(objectLight));
+    }
+    
+  }
+}
