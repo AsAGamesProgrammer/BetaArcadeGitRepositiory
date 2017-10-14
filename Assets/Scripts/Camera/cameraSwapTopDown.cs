5c1cf62036e455ccde1e57d90620b2baab82ce88
Kristina <misskristinablinova@gmail.com>
2017-10-14 17:13:07 +0100

    Camera trigger bug fixed
    - Added a second trigger for a more precise detection
    -Main camera is a private variable and is automatically detected
    - Minor commenting

---
+++ Assets/Scripts/Camera/cameraSwapTopDown.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraSwapTopDown : MonoBehaviour {
+
+    private Camera mainCamera;
+
+	// Use this for initialization
+	void Start ()
+    {
+        mainCamera = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
+    }
+	
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            mainCamera.GetComponent<CameraCave>().enabled = true;
+
+            mainCamera.GetComponent<CameraCave>().resetPosition();
+        }
+    }
+}
