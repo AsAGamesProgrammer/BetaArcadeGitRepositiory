cff0f6d5bb7d247446770c963395b1f59b7bf8c5
Unknown <misskristinablinova@gmail.com>
2017-10-18 10:15:44 +0100

    Wednesday showcase
    Minor changes, set up, water asset

--- Assets/Scripts/Camera/mapCamera.cs
+++ Assets/Scripts/Camera/mapCamera.cs
@@ -18,12 +18,22 @@ public class mapCamera : MonoBehaviour {
         mainCamera = GameObject.FindWithTag("MainCamera");
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+            print("E Pressed");
+    }
+
     private void OnTriggerStay(Collider other)
     {
+        if (Input.GetKeyDown(KeyCode.E))
+            print(other.name);
+
         if (other.gameObject.tag == "Player")
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
+                print("Camera Swap");
                 if (mainCamera.activeSelf)
                 {
                     mainCamera.SetActive(false);
