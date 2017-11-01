63eaf3c135aada3939937f526803ed9b66bbf0af
Unknown <henrybell96@hotmail.com>
2017-11-01 10:57:02 +0000

    Started On Projectiles

---
+++ Assets/Scripts/Projectiles/StreamLauncher.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StreamLauncher : MonoBehaviour {
+
+    [SerializeField]
+    private Vector3 StreamDirection = Vector3.forward;
+
+    [SerializeField]
+    [Range(0.0f, 50.0f)]
+    private float MaxDistance = 20.0f;
+
+    private Vector3 mStreamVector;
+
+
+    //-------------------------------------------Unity Functions-------------------------------------------
+
+    
+
+
+    //------------------------------------------Private Functions------------------------------------------
+
+    Vector3 CalculateStreamVector()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(this.transform.position, StreamDirection.normalized, out hit, MaxDistance))
+        {
+            // 
+        }
+        return Vector3.zero;
+    }
+}
