3765e7333fc4c40a629b62b25ebfcedd26bb4acb
Unknown <henrybell96@hotmail.com>
2017-11-09 17:51:03 +0000

    Added Conventional Projectile Launcher
    Found in resources folder

---
+++ Assets/Scripts/Camera/Obsolete/Projectile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Projectile : MonoBehaviour {
+
+    [HideInInspector]
+    public float Speed = 1.0f;
+
+    [SerializeField]
+    private float Lifetime = 5.0f;
+
+    private float mTimeOfSpawn = 0.0f;
+
+
+    //-------------------------------------------Unity Functions-------------------------------------------
+
+    private void Start()
+    {
+        mTimeOfSpawn = Time.time;
+        GetComponent<Rigidbody>().velocity = Speed * this.transform.forward;
+    }
+
+    private void Update()
+    {
+        if (Time.time - mTimeOfSpawn >= Lifetime)
+            Die();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Die();
+    }
+
+
+    //------------------------------------------Private Functions------------------------------------------
+
+    private void Die()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/Projectiles/ProjectileLauncher.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileLauncher : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject ProjectilePrefab;
+
+    [SerializeField]
+    private float ProjectileSpeed = 1.0f;
+
+    [SerializeField]
+    [Range(0.1f, 10.0f)]
+    private float TimeBetweenShots = 2.0f;
+
+    [SerializeField]
+    private Vector3 ProjectileDirection = Vector3.forward;
+
+    [SerializeField]
+    private Vector3 ProjectileSpawnOffset = Vector3.zero;
+
+    private float mTimeOfLastShot = 0.0f;
+
+    private Vector3 pProjectileDirection { get { return this.transform.TransformDirection(ProjectileDirection).normalized; } }
+    private Vector3 pProjectileSpawn { get { return this.transform.position + this.transform.TransformDirection(ProjectileSpawnOffset); } }
+
+
+    //-------------------------------------------Unity Functions-------------------------------------------
+
+    private void OnEnable()
+    {
+        mTimeOfLastShot = Time.time;
+    }
+
+    private void Update()
+    {
+        if (Time.time - mTimeOfLastShot >= TimeBetweenShots)
+            Shoot();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawSphere(pProjectileSpawn, 0.25f);
+        Gizmos.DrawLine(pProjectileSpawn, pProjectileSpawn + pProjectileDirection.normalized);
+    }
+
+
+    //------------------------------------------Private Functions------------------------------------------
+
+    private void Shoot()
+    {
+        var obj = Instantiate(ProjectilePrefab, pProjectileSpawn, ProjectilePrefab.transform.rotation) as GameObject;
+        obj.transform.LookAt(obj.transform.position + pProjectileDirection);
+        obj.GetComponent<Projectile>().Speed = ProjectileSpeed;
+        mTimeOfLastShot = Time.time;
+    }
+}
