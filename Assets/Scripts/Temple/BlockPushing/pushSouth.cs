c06ba62f39d6cbacfddde53c5abfefaf942679e5
Kristina <misskristinablinova@gmail.com>
2017-11-14 19:38:16 +0000

    Blocks detect perfectly

--- Assets/Scripts/Temple/BlockPushing/pushSouth.cs
+++ Assets/Scripts/Temple/BlockPushing/pushSouth.cs
@@ -6,16 +6,6 @@ public class pushSouth : MonoBehaviour {
 
     public GrabBlock blockScript;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
