d619a0cc5197983628f597faabb6d6c2a05290c2
Kristina <misskristinablinova@gmail.com>
2017-11-14 19:20:02 +0000

    Better grip on temple blocks
    Beginning of improving the grip on the blocks

---
+++ Assets/Scripts/Temple/BlockPushing/pushSouth.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pushSouth : MonoBehaviour {
+
+    public GrabBlock blockScript;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            blockScript.setPushedSide(GrabBlock.side.bot);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            blockScript.setPushedSide(GrabBlock.side.none);
+        }
+    }
+}
