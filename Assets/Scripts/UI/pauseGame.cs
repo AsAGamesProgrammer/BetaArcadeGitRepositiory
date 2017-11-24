4b206a5b593e577f071e70ed397dd90fe5418f89
Kristina <misskristinablinova@gmail.com>
2017-11-24 15:53:16 +0000

    Simple pause is in place

---
+++ Assets/Scripts/UI/pauseGame.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// TARGET: any empty game object
+/// Script pauses the game but not the camera. Use specific camera scripts for this task
+/// </summary>
+
+public class pauseGame : MonoBehaviour {
+
+    private bool isPaused = false;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Get input for a pause
+        if (Input.GetButtonDown("Pause"))
+        {
+            isPaused = !isPaused;
+            Debug.Log("Game paused " + isPaused);
+        }
+
+        //Handle a pause
+        if (isPaused)
+            Time.timeScale = 0f;
+        else
+            Time.timeScale = 1f;
+	}
+}
