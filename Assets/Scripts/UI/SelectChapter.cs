8605a85c13be742c124a815f63208504bccd1bf3
Craig Belshaw <craigbelshaw0@gmail.com>
2017-11-28 16:31:56 +0000

    added select sound to level select

--- Assets/Scripts/UI/SelectChapter.cs
+++ Assets/Scripts/UI/SelectChapter.cs
@@ -83,7 +83,9 @@ public class SelectChapter : MonoBehaviour {
 
                 //Move character
                 setTia(destinations[currentLocation - 1]);
-
+                var gObject = GameObject.FindGameObjectWithTag("Click");
+                var audio = gObject.GetComponent<AudioSource>();
+                audio.Play();
                 //Pla music music
                 audioSources[currentLocation - 1].Play();
 
@@ -103,7 +105,9 @@ public class SelectChapter : MonoBehaviour {
 
                 //Move character
                 setTia(destinations[currentLocation - 1]);
-
+                var gObject = GameObject.FindGameObjectWithTag("Click");
+                var audio = gObject.GetComponent<AudioSource>();
+                audio.Play();
                 //Play music
                 audioSources[currentLocation - 1].Play();
             }
