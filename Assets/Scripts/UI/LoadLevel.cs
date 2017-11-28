da64c5c22b5f4783360cad4418184c8a71f8a011
Craig Belshaw <craigbelshaw0@gmail.com>
2017-11-28 16:04:24 +0000

    added click

--- Assets/Scripts/UI/LoadLevel.cs
+++ Assets/Scripts/UI/LoadLevel.cs
@@ -7,11 +7,15 @@ public class LoadLevel : MonoBehaviour {
 
     public int nextScene = 1;
 
-    public void loadNext()
-    {
-        if (nextScene < 0)
-            Application.Quit();
-
-        SceneManager.LoadScene(nextScene);
-    }
+  public void loadNext()
+  {
+    
+    if (nextScene < 0)
+      Application.Quit();
+        
+    SceneManager.LoadScene(nextScene);
+    var gObject = GameObject.FindGameObjectWithTag("Click");
+    var audio = gObject.GetComponent<AudioSource>();
+    audio.Play();
+  }
 }
