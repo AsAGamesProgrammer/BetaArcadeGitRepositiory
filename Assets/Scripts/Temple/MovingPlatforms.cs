954f31a7c16d57841e1ccc88cb06bb2f5cf4b747
Craig Belshaw <craigbelshaw0@gmail.com>
2017-12-07 11:13:55 +0000

    added moving platforms

---
+++ Assets/Scripts/Temple/MovingPlatforms.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatforms : MonoBehaviour {
+  public Vector3 movement;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+  void OnTriggerEnter(Collider other)
+  {
+
+    if (other.gameObject.tag == "Player")
+    {
+      other.transform.parent = transform;
+
+    }
+  }
+
+  void OnTriggerExit(Collider other)
+  {
+    if (other.gameObject.tag == "Player")
+    {
+      other.transform.parent = null;
+
+    }
+  }
+
+  // Update is called once per frame
+  void FixedUpdate () {
+    transform.position+=movement * Mathf.Cos(Time.time) * Time.deltaTime;
+  }
+}
