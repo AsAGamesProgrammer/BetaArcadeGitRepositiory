563990217e2445979d7113cfbf38598a89b06cb4
Unknown <craigbelshaw0@gmail.com>
2017-12-18 19:16:32 +0000

    working opening cinematic

--- Assets/Scripts/Utility/MoviePlayer.cs
+++ Assets/Scripts/Utility/MoviePlayer.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
 [RequireComponent(typeof(AudioSource))]
 
 public class MoviePlayer : MonoBehaviour {
-  public MovieTexture movie;
-  private AudioSource audio;
+  public VideoPlayer movie;
 
   public bool transitionIsDone;
   public string nextSceneName;
@@ -16,11 +17,7 @@ public class MoviePlayer : MonoBehaviour {
   bool isPaused;
   // Use this for initialization
   void Start () {
-    GetComponent<RawImage>().texture = movie as MovieTexture;
-    audio = GetComponent<AudioSource>();
-    audio.clip = movie.audioClip;
     movie.Play();
-    audio.Play();
   }
 
   public IEnumerator ChangeSceneHelper(string sceneName)
