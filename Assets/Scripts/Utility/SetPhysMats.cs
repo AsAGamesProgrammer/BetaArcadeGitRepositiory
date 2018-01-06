a5f5afe1c34bfeb5c8c5e44f3293532e570120d4
Unknown <henrybell96@hotmail.com>
2018-01-06 17:59:30 +0000

    Automatically set physics material to no-stick on level load

---
+++ Assets/Scripts/Utility/SetPhysMats.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetPhysMats : MonoBehaviour {
+
+    [SerializeField]
+    private PhysicMaterial PhysMat;
+
+    private void Start()
+    {
+        foreach (var collider in FindObjectsOfType<Collider>())
+            collider.material = PhysMat;
+    }
+}
