cb42b5a3d13907768668a2329fb8490072e7f26d
Kristina <misskristinablinova@gmail.com>
2018-01-09 11:14:27 +0000

    Temple resets on flling platforms

---
+++ Assets/Scripts/Camera/PitfallCamera/OnTriggerRestart.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Restart level on trigger enter
+/// </summary>
+
+public class OnTriggerRestart : MonoBehaviour {
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //Debug.Log("Colliding");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
