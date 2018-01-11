402ddbcab9a6f8a9b17eb4642265eebe886fa868
Unknown <henrybell96@hotmail.com>
2018-01-11 13:32:33 +0000

    Back to main menu from chapter select

--- Assets/Scripts/UI/MainMenuBack.cs
+++ Assets/Scripts/UI/MainMenuBack.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Credits : MonoBehaviour {
+public class MainMenuBack : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
